1401bea14edbb330b8332713b046ba623ea3ac85
Glitch (subsequent-thorn-journey) <none>
2021-08-21 10:05:44 +0000

    TGBOTCIV

--- bot.js
+++ bot.js
@@ -6,11 +6,11 @@
 /*****************************************************************************************************************/
 const { Telegraf } = require("telegraf");
 const {MenuTemplate, MenuMiddleware} = require('telegraf-inline-menu')
-
-const botlink = new Telegraf(process.env.BOT_TOKEN);
-const botingresso = new Telegraf(process.env.BOT2_TOKEN);
-const chat_id = process.env.CHAT_ID;
-const chat2_id = process.env.CHAT2_ID;
+ 
+const botlink = new Telegraf(process.env.BOTLINK_TOKEN);
+const botingresso = new Telegraf(process.env.BOTINGRESSO_TOKEN);
+const chatingresso_id = process.env.CHATINGRESSO_ID;
+const chatlink_id = process.env.CHATLINK_ID;
 
 /*************************************VARIABILI IMPOSTABILI****************************************************/
 const timeExpireLink = 30; //secondi destinati all'expire del link
@@ -19,11 +19,13 @@ var memberlimit = 99999; //limite massimo di membri nella chat per accettare nuo
 
 /*******************************************VARIABILI**********************************************************/
 const secondsSinceEpoch = Math.round(Date.now() / 1000);
+const chat = process.env.CHAT_ID
 var epoch = 0;
 var linkok
 var data2
 var messageId
 var messageId2
+var countkick = 0
 var sec = secondsSinceEpoch;
 var y = timeExpireLink;
 var timedeletems = timedeletes * 1000;
@@ -39,7 +41,7 @@ botingresso.on('new_chat_member', (ctx) => {
   epoch = sec;
   var y = timeExpireLink;
   var result = epoch + y;
-  botlink.telegram.createChatInviteLink(chat2_id, { expire_date: result }, { members_limit: memberlimit }).then((data) => {
+  botlink.telegram.createChatInviteLink(chatlink_id, { expire_date: result }, { members_limit: memberlimit }).then((data) => {
   const stringjson = JSON.stringify(data);
   const obj = JSON.parse(stringjson);
   const menuTemplate = new MenuTemplate(ctx => `Hello ${ctx.from.first_name}, you have 30 seconds to press the button below and join our group. Welcome!`)
@@ -51,7 +53,9 @@ botingresso.on('new_chat_member', (ctx) => {
     return new Promise(resolve => setTimeout(resolve, ms));
     }
   async function delayKick() {
-
+                            
+                            
+             countkick = countkick +1;
              await sleep(timedeletems);
              messageId = ctx.message.message_id
              messageId2 = ctx.message.message_id + 1
@@ -63,13 +67,29 @@ botingresso.on('new_chat_member', (ctx) => {
     return 0
     }
   delayKick(); 
-  
   })
   
   return false
   
   })
+             botingresso.command('kicked', (ctx) => {ctx.telegram.sendMessage(ctx.chat.id, 'Bot settled: ' + countkick )})
+             botingresso.command('systemstate', (ctx) => {ctx.telegram.sendMessage(ctx.chat.id, 'Online')})
+             botlink.command('systemstate', (ctx) => {ctx.telegram.sendMessage(ctx.chat.id, 'Online')})
+//****************************************************ONLY FOR TEST PURPOSE*****************************************************//
+              /*botlink.command('test', (ctx) => {
+               
+              const secondsSinceEpoch = Math.round(Date.now() / 1000);
+              var sec = secondsSinceEpoch;
+              epoch = sec;
+              var y = timeExpireLink; 
+              var result = epoch + y;
+              botlink.telegram.createChatInviteLink(chatlink_id, { expire_date: result }, { members_limit: memberlimit }).then((data) => {
+              const stringjson = JSON.stringify(data);
+              const obj = JSON.parse(stringjson);
+              ctx.telegram.sendMessage(ctx.chat.id, obj.invite_link) 
+             })})*/
+
 //***************************************************************************************************************
-  botlink.launch()
+  botlink.launch() 
   botingresso.launch()
 //***************************************************************************************************************
